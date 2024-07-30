27070adb5bc0ed3c82e29bddb1cd07765167e910
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-07-30 11:23:41 +0530

    01

---
+++ 1index.js
@@ -0,0 +1,11 @@
+console.log("Hello akash anuragi")
+// console.log is used for run the function
+
+var a =54 //a contain 54
+console.log(a)
+a="Akash"
+console.log(a)
+// javascript reserved words cannot be used as variable name like 'var' EventCounts.
+a="akash"
+console.log(a)
+// Akash &akash both are different variable
