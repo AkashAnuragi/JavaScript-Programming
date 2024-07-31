3e21a34f79fa52f281eff950f32132ed35f8aeed
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-07-31 17:46:03 +0530

    Add files via upload

---
+++ 3datatype.js
@@ -0,0 +1,20 @@
+// today topic is primitive datatypes and objects
+// there are 7 types of premitive datatype: null,number,symbol,string,boolean,bigint,undefined
+let a = null;
+let b = 45;
+let c = "akash" 
+let d = true;
+let e = BigInt("34") + BigInt("34")
+let f = Symbol("i am a nice Symbol")
+let g = undefined 
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
+
+// OBJECTS-non-primitive datatype.
+const item ={
+    "Akash":true,
+    "Ayushi":false,
+    "Ayush":56,
+    "Kartik":undefined
+}
+console.log(item["Ayush"])
