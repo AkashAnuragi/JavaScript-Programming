5255a5c268e7c3098ee46afa162264c4f6c65918
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-08-02 22:33:24 +0530

    04

---
+++ 4Exercise1.js
@@ -0,0 +1,28 @@
+// Chapter 1 
+// practice set
+let a = 45
+a ="Akash"
+let  b =23
+console.log(b+a)
+
+console.log(typeof(a+b))
+
+const a1 = {
+    name :"Akash",
+    section: 1,
+    isPrincipal : false
+}
+// a1= 34
+a1['friend'] ="ayushi"
+a1['name'] = "rajan"
+console.log(a1)
+
+const dict = {
+    pogonophile : "a person who likes beards.",
+    cleek :"to grasp or seize suddenly.",
+    pawky :"cunning; sly.",
+    institute :"set up or lay the groundwork for",
+    
+    affect :"have an influence upon"
+}
+console.log(dict)
