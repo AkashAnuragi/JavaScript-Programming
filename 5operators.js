5c132edfa6ac289df561a3473f73ac9b80b563b2
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-08-03 15:13:14 +0530

    05

---
+++ 5operators.js
@@ -0,0 +1,38 @@
+//Expression and  conditions
+console.log("Operators in JS")
+let a =23 ;
+let b=2 ;
+//Arithmetic Operators
+console.log("a+b=",a+b) //Addition
+console.log("a-b=",a-b) //SUbtraction
+console.log("a*b=",a*b) //Multiplication 
+console.log("a/b=",a/b) //Division
+console.log("a%b=",a%b) //modulus
+console.log("a**b=",a**b)//exponatial
+console.log("++a=",++a)//Increment
+console.log("a++=",a++)//Increment
+console.log("a=",a)//Equal to
+console.log("--aa =",--a)//Decrement
+console.log("a-- =",a--)//Decrement
+console.log("a=",a)//Equal to
+
+//Assignment operators
+// =,+=, -=,*=,/=,**=etc
+ 
+//Comparison operators
+let comp1 =1
+let comp2 =2
+let comp3="1"
+console.log(comp1==comp2)
+console.log(comp1!==comp2)
+console.log(comp1===comp3) //Equal to and type 
+
+// Logical operator
+let lg =4
+let lg1 =5
+console.log(lg<lg1 && lg==lg1)
+console.log(lg<lg1 || lg==lg1)
+console.log(!true)
+
+/*Cmments for  large sentences*/
+// Cmments
