7dfd9b7cee096a695371dee0257c749d9aa66e1d
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-08-04 19:16:45 +0530

    06

---
+++ 6Conditional.js
@@ -0,0 +1,11 @@
+//Chapter 2 practice set
+//1. use the logical oprators to find whether the age of a person lies between 10 and 20.
+// let age = prompt("What's your age?")
+let age = 10;
+if (age>10 && age<20){
+  console.log("your age is lies between the 10 and 20 ")
+}
+else{
+  console.log("does not lies between the 10 and 20")
+}
+//prompt are not run in the vs code it is run in the browser .
