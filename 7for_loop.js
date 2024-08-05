b08907d74f5114116555a24b3dd89cc57b6c2aef
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-08-05 20:30:03 +0530

    07

---
+++ 7for_loop.js
@@ -0,0 +1,27 @@
+//here we  learn about the loops and functions
+//program to add first some natural numbers 
+// let sum=0;
+// let n =prompt("Enter the value of n :")
+// n = Number.parseInt(n)
+// for (let i =0; i<n ;i++)
+// {
+//     sum += (i+1)
+// }
+//     console.log(" Sum of first " + n + " natural number is " + sum)
+
+
+//for in loop
+// let obj = {
+//     akash : 12,
+//     ayushi :  23,
+//     raaj : 45,
+//     rajan : 56
+// }
+// for(let a in obj){
+//     console.log("markes of " + a + " are " + obj[a])
+// }
+
+//For of Loop
+for (let b of "akash"){
+    console.log(b)
+}
