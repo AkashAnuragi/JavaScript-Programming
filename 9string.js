7f675fbfff6d6bfae4fb4070e571ae7fe23741da
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-08-07 20:34:58 +0530

    09

---
+++ 9string.js
@@ -0,0 +1,27 @@
+console.log("This is strings tutorial")
+let a = "Akash";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log("the lenght of string is :",a.length)
+
+let real_name = "Akash"
+let friend = "Ayushi"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "Vashnavi"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log("the length of b string is :",b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("vi", "44"))
+console.log(b.concat(a, b, friend, "Priya"))
+
+console.log(b)
